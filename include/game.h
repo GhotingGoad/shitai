5380657b0bac80de523d6a887f39683cd94505a6
GhotingGoad <rodohzhihua@gmail.com>
2018-08-06 22:08:02 +0800

    Add files via upload

--- include/game.h
+++ include/game.h
@@ -2,17 +2,10 @@
 #define GAME_H_INCLUDED
 
 
-
-bool initializeGame(int SCREEN_WIDTH, int SCREEN_HEIGHT, bool flagError);
-bool loadMedia(bool flagError);
+void initializeGame(int SCREEN_WIDTH, int SCREEN_HEIGHT);
+void loadMedia();
 SDL_Surface* loadSurface();
 void refreshWindow();
 void quit();
-
-void moveDown();
-void moveLeft();
-void moveRight();
-void rotateClockwise();
-void rotateCounterClockwise();
 
 #endif // GAME_H_INCLUDED
